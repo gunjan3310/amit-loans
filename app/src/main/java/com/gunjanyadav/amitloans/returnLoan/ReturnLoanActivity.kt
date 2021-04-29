a5cf2feecdc64597525ea03c627d4da9b144dca6
Gunjan Yadav <gunjanyadav@gunjanyadav.security-station>
2021-04-28 21:50:02 -0400

    payment recieve feature added

---
+++ app/src/main/java/com/gunjanyadav/amitloans/returnLoan/ReturnLoanActivity.kt
@@ -0,0 +1,12 @@
+package com.gunjanyadav.amitloans.returnLoan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gunjanyadav.amitloans.R
+
+class ReturnLoanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_return_loan)
+    }
+}
